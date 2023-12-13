18aed39436021433bf13d566fac2965a2cc75790
ytran <a11718trannhuy@gmail.com>
2023-12-13 12:15:58 +0700

    update code

---
+++ 605. Can Place Flowers.js
@@ -0,0 +1,31 @@
+var canPlaceFlowers = function(flowerbed, n) {
+    if (flowerbed.length === 1 && flowerbed[0] === 0) return true;
+    const copy = flowerbed;
+    let count = 0;
+
+    for (let i = 0; i < copy.length; i++) {
+        if (count === n) break;
+        if (i === 0 && copy[i + 1] === 0 && copy[i] === 0) {
+            // console.log("1");
+            copy[i] = 1;
+            count++;
+        } else if (i === copy.length - 1 && copy[i - 1] === 0 && copy[i] === 0) {
+            // console.log("2");
+            copy[i] = 1;
+            count++;
+        } else if (copy[i] !== 1 && copy[i - 1] === 0 && copy[i + 1] === 0) {
+            // console.log("3");
+            copy[i] = 1;
+            count++;
+        };
+    };
+
+    return count === n;
+};
+
+const flowerbed = [0,0,1,0,0];
+const n = 1;
+
+const result = canPlaceFlowers(flowerbed, n);
+
+console.log(result);

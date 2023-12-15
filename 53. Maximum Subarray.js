97a2f5e1d27af2fcdc6cb2fa5c84b5d09b817b19
ytran <a11718trannhuy@gmail.com>
2023-12-15 16:15:04 +0700

    solve problem 53

---
+++ 53. Maximum Subarray.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let max = -Infinity;
+    let sum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        max = Math.max(max, sum);
+
+        if (sum < 0) sum = 0;
+    };
+
+    return max;
+};
+
+const nums = [-2,1,-3,4,-1,2,1,-5,4];
+
+const result = maxSubArray(nums);
+
+console.log(result);
